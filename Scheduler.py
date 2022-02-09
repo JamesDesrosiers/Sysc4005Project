b7ec969e06c60c26755c4884828ea1680bb1d777
James Desrosiers <JamesDavidDesrosiers@gmail.com>
2022-02-09 15:27:28 -0500

    Adding a generic event object

--- Scheduler.py
+++ Scheduler.py
@@ -5,17 +5,37 @@ class scheduler:
     
     eventList = None
     time = 0
+
+    productsMade = 0
+
+    entities = []
     
     def __init__():
         eventList = []
 
-    def addEvent(new):
-        eventList.append(new)
-        sortEvents()
+    def addEvent(self.new):
+        self.eventList.append(new)
+        self.sortEvents()
 
     def getTime(e):
         return e.time
 
-    def sortEvents():
+    def sortEvents(self):
         eventList.sort(key = getTime)
-5
+
+    def logEvent(e):
+        pass
+
+    def __main__():
+        pass
+
+class event:
+    time = 0
+    handler = None
+
+    def __init__(creator, finish):
+        time = finish
+        handler = creator
+
+    def handle(self):
+        creator.handle(self)
