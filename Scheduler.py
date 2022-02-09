202213bebbb330e57b7d8a30ec64fec0119c94d9
James Desrosiers <JamesDavidDesrosiers@gmail.com>
2022-02-09 15:50:21 -0500

    Simulation Loop
    Created the main function to instance the simulation. Also did some bug fixing and some QoL changes.

--- Scheduler.py
+++ Scheduler.py
@@ -1,7 +1,7 @@
 
 
 
-class scheduler:
+class Scheduler:
     
     eventList = None
     time = 0
@@ -10,10 +10,10 @@ class scheduler:
 
     entities = []
     
-    def __init__():
-        eventList = []
+    def __init__(self):
+        self.eventList = []
 
-    def addEvent(self.new):
+    def addEvent(self, new):
         self.eventList.append(new)
         self.sortEvents()
 
@@ -21,21 +21,59 @@ class scheduler:
         return e.time
 
     def sortEvents(self):
-        eventList.sort(key = getTime)
+        self.eventList.sort(key = getTime)
 
-    def logEvent(e):
-        pass
+    def idleCheck(self):
+        checks = 0
+        active = 0
+        for i in self.entities:
+            checks += 1
+            if i.state == IDLE:
+                i.activate(self, self.time)
+                active += 1
+        logEvent(str(checks) + " entites checked for idleness")
+        logEvent(str(active) + " entites have been activated")
+        return
 
-    def __main__():
-        pass
+    def popEvent(self):
+        if len(self.eventList) == 0:
+                logEvent("There exists no Events to Process")
+                return -1
+        x = self.eventList.pop(0)
+        return x
+
+
 
 class event:
     time = 0
     handler = None
 
-    def __init__(creator, finish):
-        time = finish
-        handler = creator
+    def __init__(self, creator, finish):
+        self.time = finish
+        self.handler = creator
 
     def handle(self):
-        handler.handle(self)
+        self.handler.handle(self)
+
+def logEvent(l):
+    print(l)
+    
+def main():
+    #Create the Scheduler
+    schedule = Scheduler()
+
+    #Create the Entities
+    #schedule.entities.append(inspector())
+
+    #Add Initial events(Via the Idle Check)
+    schedule.idleCheck()
+
+    #Simulation loop
+    code = -1
+    while code != -1:
+        #Go to next event
+        x = schedule.popEvent()
+    print("Simulation Ended")
+
+if __name__ == "__main__":
+    main()
