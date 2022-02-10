022f827f0dfd07e871759f3da361b5047d42dfae
James Desrosiers <JamesDavidDesrosiers@gmail.com>
2022-02-10 14:44:32 -0500

    Updating Inspector
    Moved to use the scheduler and not real time simulation.
    
    Formatted the code to make future needed changes easier.

--- Inspector.py
+++ Inspector.py
@@ -1,15 +1,25 @@
 import Workstate
 import time
+from Scheduler import event
+import Component
 
 class Inspector:
+    #Variables for State
     state = None
     buffers = None
     component = None
+    stuckBuffer = None
+    scheduler = None
 
-    def __init__(self, bf, ctype):
+    #TEMP Variables for working
+    #TODO find duration based on data
+    duration = 2
+
+    def __init__(self, bf, ctype, sch):
         self.buffers = bf
         self.state = Workstate.IDLE
         self.component = ctype
+        self.scheduler = sch
 
     def get_state(self):
         return self.state
@@ -17,16 +27,44 @@ class Inspector:
     def get_id(self):
         return self.id
 
+    #TODO Finish code for Inpector 2's handle
     def handle(self, event):
-        self.state = Workstate.BUSY
-        time.sleep(event.time)
+        if component == Component.C1:
+            # Code that Finds the smallest buffer
+            smallest_buffer = self.buffers[0]
+            for buffer in self.buffers:
+                if buffer.get_length() < smallest_buffer.get_length():
+                    smallest_buffer = buffer
+            
+            #Checking if the buffer can have something pushed
+            if smallest_buffer.isFull():
+                self.stuckBuffer = smallest_Buffer
+                self.state = Workstate.IDLE
+            else:
+                self.smallest_buffer.add_component(self.component)
+                self.beginWork()
+        elif isinstance(component, list):
+            #Add Code for doing inspector 2 here
+            pass
+            
 
-        # After sleep, send component to workstation
-        smallest_buffer = self.buffers[0]
-        for buffer in self.buffers:
-            if buffer.get_length() < smallest_buffer.get_length():
-                smallest_buffer = buffer
+    #Fuction that adds a finish event to be handled
+    def beginWork(self):
+        temp = event(self, duration + scheduler.time)
+        scheduler.addEvent(temp)
+        self.state = Workstate.busy
 
-        smallest_buffer.add_component(self.component)
-
-        self.state = Workstate.IDLE
+    #There exists 2 reasons for an inspector to be idle
+    #   1- The inspector has yet to become busy for the first time
+    #   2- The inspector is stuck on pushing out a component
+    def activate(self):
+        if self.stuckBuffer is not None:
+            if self.stuckBuffer.isFull():
+                #The Inspector is still stuck
+                return
+            else:
+                self.stuckBuffer.add_component(self.component)
+                self.beginWork()
+        else:
+            self.beginWork()
+        
